f3078510a14c8bef9c6f9d41f185926b784eeb31
Jordan Bobadilla <jordanboba08@gmail.com>
2021-06-16 20:58:09 -0400

    Home page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
+import { Route, Switch } from 'react-router';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <main className="App">
       <NavBar/>
+      <Switch>
+        <Route exact path={"/"} component={Home}/>
+      </Switch>
     </main>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import "../styles/Home.css"
+
+const Home = () => {
+    return (
+        <section className="home">
+            <h1>Welcome to the Budget App</h1>
+            <p>Click the logo to see your transactions.</p>
+        </section>
+    )
+}
+
+export default Home
